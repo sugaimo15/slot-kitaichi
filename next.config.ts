843547a3bcffca731cfaf553f8a446f158c6d4a2
sugaimo15 <75659327+sugaimo15@users.noreply.github.com>
2026-05-23 15:40:41 +0900

    Fix basePath for production environment

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/slot-kitaiti" : "",
+  basePath: process.env.NODE_ENV === "production" ? "/slot-kitaichi" : "",
   images: { unoptimized: true },
 };
 
